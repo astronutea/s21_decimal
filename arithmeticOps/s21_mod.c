ac0f60b493b1d98a9458da1076a9c4d759b899b9
Antony Lott <antonylo@pr-c4.kzn.21-school.ru>
2025-05-12 18:23:43 +0300

    div remastered

--- arithmeticOps/s21_mod.c
+++ arithmeticOps/s21_mod.c
@@ -2,29 +2,13 @@
 
 int s21_mod(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     s21_null_decimal(result);
-    int status = 0;
-
-    if (value_2.bits[0] == 0 && value_2.bits[1] == 0 && value_2.bits[2] == 0) {
-        return 3;
-    }
-
-    s21_decimal quotient = {0};
-    s21_decimal int_part = {0};
-    s21_decimal multiplied = {0};
-    s21_decimal temp = {0};
-
-    s21_div(value_1, value_2, &quotient);
-    s21_truncate(quotient, &int_part);
-    s21_mul(int_part, value_2, &multiplied);
-    status = s21_sub(&value_1, &multiplied, &temp);
-
-    if (status == 1) {
-        s21_truncate(temp, result);
-        status = 0;
-    } else {
-        *result = temp;
-    }
-
-    s21_set_sign(result, s21_get_sign(&value_1));
-    return status;
+    s21_decimal div_result, mul_result;
+    s21_decimal a = value_1, b = value_2;
+    int sign = s21_get_sign(&value_1);
+    s21_set_sign(&b, 0);
+    s21_div(a, b, &div_result);
+    s21_set_sign(&div_result, 0);
+    s21_mul(div_result, b, result);
+    s21_set_sign(&result, sign);
+    return 0;
 }
