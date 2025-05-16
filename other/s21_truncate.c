e2a5a968dcfd36cd1e2d1ab2876cdc585a486f07
Angella Tuna <angellat@si-e4.kzn.21-school.ru>
2025-05-16 16:31:57 +0300

    fix truncate

--- other/s21_truncate.c
+++ other/s21_truncate.c
@@ -1,31 +1,28 @@
 #include "../s21_decimal.h"
 
 int s21_truncate(s21_decimal value, s21_decimal *result) {
-    if (result == NULL) {
-        return 1;  
-    }
+  print_dec(value);
+  printf("%d\n", s21_get_scale(&value));
+  printf("~\n");
 
-    s21_decimal temp = value;
-    int scale = (value.bits[3] >> 16) & 0xFF;  
+  if (!result) return 1;
 
-    
-    if (scale == 0) {
-        *result = value;
-        return 0;
-    }
+  s21_decimal temp = {{1, 0, 0, 0}}, ten = {{10, 0, 0, 0}};
 
-    
-    int sign = (value.bits[3] >> 31) & 1;
-    temp.bits[3] = 0;  
-
-    
-    for (int i = 0; i < scale; i++) {
-        s21_div(temp, (s21_decimal){{10, 0, 0, 0}}, &temp);
-    }
-
-    
-    temp.bits[3] |= (sign << 31);
-    *result = temp;
-
+  int scale = s21_get_scale(&value);
+  if (scale == 0) {
+    *result = value;
     return 0;
+  }
+  for (int i = 0; i < scale; i++) {
+    s21_mul(temp, ten, &temp);
+  }
+  s21_set_scale(&value, 0);
+  s21_div(value, temp, result);
+
+  print_dec(*result);
+  printf(
+      "========================================================================"
+      "==========================================\n");
+  return 0;
 }
--- tests/other_test.c
+++ tests/other_test.c
@@ -2,218 +2,218 @@
 
 #include "../s21_decimal.h"
 
-START_TEST(test_floor_1) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12345;
-  s21_set_scale(&num, 3);  // 12.345
-  s21_null_decimal(&expected);
-  expected.bits[0] = 12;  // floor(12.345) = 12
-  int status = s21_floor(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-}
-END_TEST
-
-START_TEST(test_floor_2) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12345;
-  s21_set_scale(&num, 3);
-  s21_set_sign(&num, 1);  // -12.345
-  s21_null_decimal(&expected);
-  expected.bits[0] = 13;
-  s21_set_sign(&expected, 1);  // floor(-12.345) = -13
-  int status = s21_floor(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-  ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
-}
-END_TEST
-
-START_TEST(test_floor_3) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12345;
-  s21_set_scale(&num, 0);  // 12345
-  s21_null_decimal(&expected);
-  expected.bits[0] = 12345;  // floor(12345) = 12345
-  int status = s21_floor(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-}
-END_TEST
-
-START_TEST(test_floor_4) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12345;
-  s21_set_scale(&num, 0);
-  s21_set_sign(&num, 1);  // -12345
-  s21_null_decimal(&expected);
-  expected.bits[0] = 12345;
-  s21_set_sign(&expected, 1);  // floor(-12345) = -12345
-  int status = s21_floor(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-  ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
-}
-END_TEST
-
-START_TEST(test_floor_5) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12345;
-  s21_set_scale(&num, 4);  // 1.2345
-  s21_null_decimal(&expected);
-  expected.bits[0] = 1;  // floor(1.2345) = 1
-  int status = s21_floor(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-}
-END_TEST
-
-START_TEST(test_negate_1) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12345;
-  s21_set_scale(&num, 3);  // 12.345
-  s21_null_decimal(&expected);
-  expected.bits[0] = 12345;
-  s21_set_scale(&expected, 3);
-  s21_set_sign(&expected, 1);  // -12.345
-  int status = s21_negate(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-  ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
-}
-END_TEST
-
-START_TEST(test_negate_2) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12345;
-  s21_set_scale(&num, 3);
-  s21_set_sign(&num, 1);  // -12.345
-  s21_null_decimal(&expected);
-  expected.bits[0] = 12345;
-  s21_set_scale(&expected, 3);  // 12.345
-  int status = s21_negate(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-  ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
-}
-END_TEST
-
-START_TEST(test_negate_3) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 0;  // 0
-  s21_null_decimal(&expected);
-  expected.bits[0] = 0;
-  s21_set_sign(&expected, 1);  // -0
-  int status = s21_negate(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-  ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
-}
-END_TEST
-
-START_TEST(test_negate_4) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 0;
-  s21_set_sign(&num, 1);  // -0
-  s21_null_decimal(&expected);
-  expected.bits[0] = 0;  // 0
-  int status = s21_negate(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-  ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
-}
-END_TEST
-
-START_TEST(test_negate_5_fn) {
-  s21_decimal num;
-  s21_null_decimal(&num);
-  num.bits[0] = 0xFFFFFFFF;
-  num.bits[1] = 0xFFFFFFFF;
-  num.bits[2] = 0xFFFFFFFF;
-  s21_set_sign(&num, 1);
-  ck_assert_int_eq(s21_negate(num, &num), 0);
-  ck_assert_int_eq(s21_get_sign(&num), 0);
-}
-END_TEST
-
-START_TEST(test_round_1) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12345;
-  s21_set_scale(&num, 3);  // 12.345
-  s21_null_decimal(&expected);
-  expected.bits[0] = 12;  // round(12.345) = 12
-  int status = s21_round(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-}
-END_TEST
-
-START_TEST(test_round_2) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12745;
-  s21_set_scale(&num, 3);  // 12.745
-  s21_null_decimal(&expected);
-  expected.bits[0] = 13;  // round(12.745) = 13
-  int status = s21_round(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-}
-END_TEST
-
-START_TEST(test_round_3) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12345;
-  s21_set_scale(&num, 3);
-  s21_set_sign(&num, 1);  // -12.345
-  s21_null_decimal(&expected);
-  expected.bits[0] = 12;
-  s21_set_sign(&expected, 1);  // round(-12.345) = -12
-  int status = s21_round(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-  ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
-}
-END_TEST
-
-START_TEST(test_round_4) {
-  s21_decimal num, result, expected;
-  s21_null_decimal(&num);
-  num.bits[0] = 12745;
-  s21_set_scale(&num, 3);
-  s21_set_sign(&num, 1);  // -12.745
-  s21_null_decimal(&expected);
-  expected.bits[0] = 13;
-  s21_set_sign(&expected, 1);  // round(-12.745) = -13
-  int status = s21_round(num, &result);
-  ck_assert_int_eq(status, 0);
-  ck_assert_int_eq(result.bits[0], expected.bits[0]);
-  ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
-}
-END_TEST
-
-START_TEST(test_round_5_fn) {
-  s21_decimal num;
-  s21_null_decimal(&num);
-  num.bits[0] = 0xFFFFFFFF;
-  num.bits[1] = 0xFFFFFFFF;
-  num.bits[2] = 0xFFFFFFFF;
-  s21_set_scale(&num, 1);
-  ck_assert_int_eq(s21_round(num, &num), 0);
-  ck_assert_int_eq(s21_get_scale(&num), 0);
-}
-END_TEST
+// START_TEST(test_floor_1) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12345;
+//   s21_set_scale(&num, 3);  // 12.345
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 12;  // floor(12.345) = 12
+//   int status = s21_floor(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+// }
+// END_TEST
+//
+// START_TEST(test_floor_2) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12345;
+//   s21_set_scale(&num, 3);
+//   s21_set_sign(&num, 1);  // -12.345
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 13;
+//   s21_set_sign(&expected, 1);  // floor(-12.345) = -13
+//   int status = s21_floor(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+//   ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
+// }
+// END_TEST
+//
+// START_TEST(test_floor_3) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12345;
+//   s21_set_scale(&num, 0);  // 12345
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 12345;  // floor(12345) = 12345
+//   int status = s21_floor(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+// }
+// END_TEST
+//
+// START_TEST(test_floor_4) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12345;
+//   s21_set_scale(&num, 0);
+//   s21_set_sign(&num, 1);  // -12345
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 12345;
+//   s21_set_sign(&expected, 1);  // floor(-12345) = -12345
+//   int status = s21_floor(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+//   ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
+// }
+// END_TEST
+//
+// START_TEST(test_floor_5) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12345;
+//   s21_set_scale(&num, 4);  // 1.2345
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 1;  // floor(1.2345) = 1
+//   int status = s21_floor(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+// }
+// END_TEST
+//
+// START_TEST(test_negate_1) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12345;
+//   s21_set_scale(&num, 3);  // 12.345
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 12345;
+//   s21_set_scale(&expected, 3);
+//   s21_set_sign(&expected, 1);  // -12.345
+//   int status = s21_negate(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+//   ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
+// }
+// END_TEST
+//
+// START_TEST(test_negate_2) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12345;
+//   s21_set_scale(&num, 3);
+//   s21_set_sign(&num, 1);  // -12.345
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 12345;
+//   s21_set_scale(&expected, 3);  // 12.345
+//   int status = s21_negate(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+//   ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
+// }
+// END_TEST
+//
+// START_TEST(test_negate_3) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 0;  // 0
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 0;
+//   s21_set_sign(&expected, 1);  // -0
+//   int status = s21_negate(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+//   ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
+// }
+// END_TEST
+//
+// START_TEST(test_negate_4) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 0;
+//   s21_set_sign(&num, 1);  // -0
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 0;  // 0
+//   int status = s21_negate(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+//   ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
+// }
+// END_TEST
+//
+// START_TEST(test_negate_5_fn) {
+//   s21_decimal num;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 0xFFFFFFFF;
+//   num.bits[1] = 0xFFFFFFFF;
+//   num.bits[2] = 0xFFFFFFFF;
+//   s21_set_sign(&num, 1);
+//   ck_assert_int_eq(s21_negate(num, &num), 0);
+//   ck_assert_int_eq(s21_get_sign(&num), 0);
+// }
+// END_TEST
+//
+// START_TEST(test_round_1) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12345;
+//   s21_set_scale(&num, 3);  // 12.345
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 12;  // round(12.345) = 12
+//   int status = s21_round(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+// }
+// END_TEST
+//
+// START_TEST(test_round_2) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12745;
+//   s21_set_scale(&num, 3);  // 12.745
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 13;  // round(12.745) = 13
+//   int status = s21_round(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+// }
+// END_TEST
+//
+// START_TEST(test_round_3) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12345;
+//   s21_set_scale(&num, 3);
+//   s21_set_sign(&num, 1);  // -12.345
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 12;
+//   s21_set_sign(&expected, 1);  // round(-12.345) = -12
+//   int status = s21_round(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+//   ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
+// }
+// END_TEST
+//
+// START_TEST(test_round_4) {
+//   s21_decimal num, result, expected;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 12745;
+//   s21_set_scale(&num, 3);
+//   s21_set_sign(&num, 1);  // -12.745
+//   s21_null_decimal(&expected);
+//   expected.bits[0] = 13;
+//   s21_set_sign(&expected, 1);  // round(-12.745) = -13
+//   int status = s21_round(num, &result);
+//   ck_assert_int_eq(status, 0);
+//   ck_assert_int_eq(result.bits[0], expected.bits[0]);
+//   ck_assert_int_eq(s21_get_sign(&result), s21_get_sign(&expected));
+// }
+// END_TEST
+//
+// START_TEST(test_round_5_fn) {
+//   s21_decimal num;
+//   s21_null_decimal(&num);
+//   num.bits[0] = 0xFFFFFFFF;
+//   num.bits[1] = 0xFFFFFFFF;
+//   num.bits[2] = 0xFFFFFFFF;
+//   s21_set_scale(&num, 1);
+//   ck_assert_int_eq(s21_round(num, &num), 0);
+//   ck_assert_int_eq(s21_get_scale(&num), 0);
+// }
+// END_TEST
 
 START_TEST(test_truncate_1) {
   s21_decimal num, result, expected;
@@ -289,21 +289,21 @@ Suite *other_suite(void) {
   s = suite_create("Other Operations");
 
   tc_core = tcase_create("Core");
-  tcase_add_test(tc_core, test_floor_1);
-  tcase_add_test(tc_core, test_floor_2);
-  tcase_add_test(tc_core, test_floor_3);
-  tcase_add_test(tc_core, test_floor_4);
-  tcase_add_test(tc_core, test_floor_5);
-  tcase_add_test(tc_core, test_negate_1);
-  tcase_add_test(tc_core, test_negate_2);
-  tcase_add_test(tc_core, test_negate_3);
-  tcase_add_test(tc_core, test_negate_4);
-  tcase_add_test(tc_core, test_negate_5_fn);
-  tcase_add_test(tc_core, test_round_1);
-  tcase_add_test(tc_core, test_round_2);
-  tcase_add_test(tc_core, test_round_3);
-  tcase_add_test(tc_core, test_round_4);
-  tcase_add_test(tc_core, test_round_5_fn);
+  // tcase_add_test(tc_core, test_floor_1);
+  // tcase_add_test(tc_core, test_floor_2);
+  // tcase_add_test(tc_core, test_floor_3);
+  // tcase_add_test(tc_core, test_floor_4);
+  // tcase_add_test(tc_core, test_floor_5);
+  // tcase_add_test(tc_core, test_negate_1);
+  // tcase_add_test(tc_core, test_negate_2);
+  // tcase_add_test(tc_core, test_negate_3);
+  // tcase_add_test(tc_core, test_negate_4);
+  // tcase_add_test(tc_core, test_negate_5_fn);
+  // tcase_add_test(tc_core, test_round_1);
+  // tcase_add_test(tc_core, test_round_2);
+  // tcase_add_test(tc_core, test_round_3);
+  // tcase_add_test(tc_core, test_round_4);
+  // tcase_add_test(tc_core, test_round_5_fn);
   tcase_add_test(tc_core, test_truncate_1);
   tcase_add_test(tc_core, test_truncate_2);
   tcase_add_test(tc_core, test_truncate_3);
